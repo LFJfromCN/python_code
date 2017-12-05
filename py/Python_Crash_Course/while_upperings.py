947329efad9df8eef6645344a63c59c39a69e482
LFJ <15894500833@163.com>
2017-12-05 17:46:03 +0800

    new

---
+++ py/Python_Crash_Course/while_upperings.py
@@ -0,0 +1,8 @@
+uppering = ' '
+while uppering!= 'q':
+    uppering = input('Enter your favorite uppering please,enter "q" to quit: ')
+    print('Got it, we will add ' + uppering + ' to your pizza!')
+    continue
+
+
+        
