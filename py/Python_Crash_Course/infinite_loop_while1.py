fbbe68021f003d47b3061767578a242fbe56e346
LFJ <15894500833@163.com>
2017-12-10 10:42:45 +0800

    update

---
+++ py/Python_Crash_Course/infinite_loop_while1.py
@@ -0,0 +1,5 @@
+x = 'I love you!'
+while True:
+    print(x)
+
+#Don't laught at me,this is a practice from 'python crash course'. ：).
---
+++ py/Python_Crash_Course/pets_whileloop_remove.py
@@ -0,0 +1,6 @@
+pets = ['cat','dog','parrot','snake','cat','monkey','cat','rabbit']
+print(pets)
+
+while 'cat' in pets:
+    pets.remove('cat')
+print(pets)
