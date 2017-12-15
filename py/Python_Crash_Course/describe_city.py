7ac529c41dcb17e5aca07eeb22b54c306dfbdd48
LFJ <15894500833@163.com>
2017-12-15 20:06:14 +0800

    update ：）

---
+++ py/Python_Crash_Course/describe_city.py
@@ -0,0 +1,7 @@
+def describe_city(city = 'dali',nation = 'china' ):
+    print(city.title() + ' is in ' + nation.title() + ' . ')
+
+describe_city()
+describe_city('reykjavik','iceland')
+describe_city('beijing')
+describe_city('tokyo','japan')
---
+++ py/Python_Crash_Course/make_shirt.py
@@ -0,0 +1,8 @@
+def make_shirt(size =' L',color = 'black',print_letter = 'I love python'):
+    print('\nYou T-shirt is ' + size +' size, and the color is ' + color +
+          ', and the letters you want to print on it is ' + print_letter + ' . ')
+
+make_shirt('S','red','I love U')
+make_shirt('L','blue','HAHA!')
+make_shirt('XXL','light green','look at me.')
+make_shirt(color = 'orange')
