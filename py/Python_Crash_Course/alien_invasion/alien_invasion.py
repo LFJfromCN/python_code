b2dfc69b1aa3913fbcda97911dbfb217b67f1eeb
joyDDT <15894500833@163.com>
2018-03-31 21:00:12 +0800

    update
    开学第一传

---
+++ py/Python_Crash_Course/alien_invasion/alien_invasion.py
@@ -0,0 +1,35 @@
+import pygame
+from settings import Settings
+from ship import Ship
+from alien import Alien
+import game_functions as gf
+from pygame.sprite import Group
+
+
+
+def run_game( ):
+    #初始化pygame、设置和屏幕对象
+    pygame.init( )
+    ai_settings = Settings( )
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    #创建一艘飞船
+    ship = Ship(ai_settings, screen)
+    #创建一个用于存储子弹的编组，一艘飞船和一个外星人编组
+    bullets = Group( )
+    aliens = Group( )
+
+    #创建一群外星人
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    #开始游戏的主循环
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update( )
+        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+        gf.update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets )
+
+run_game( )
